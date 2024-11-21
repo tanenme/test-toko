a6b20892072edab4b56aa99b2c7291c37058075a
tanenme <tanenme@gmail.com>
2024-11-21 12:27:50 +0700

    validasi scret key jwt

--- middleware/authMiddlewaree.js
+++ middleware/authMiddlewaree.js
@@ -1,24 +1,26 @@
-import Users from "../models/usersModel.js";
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken'
+dotenv.config({ path: './.env' });
+const secret = process.env.SECRET_KEY
 
 export const authMiddleware = async (req, res, next) => {
-    const token = req.get('Authorization');
+    let token = req.get('Authorization');
     if (!token) {
         res.status(401).json({
             errors: "Unauthorized"
         }).end();
     } else {
-        const user = await Users.findOne({
-            where: {
-                token: token
+
+        token = token.split(" ")[1]
+
+        jwt.verify(token, secret, (err) => {
+            if (err) {
+                res.status(403).json({ errors: 'Invalid token' }).end();
+                return;
+            } else {
+                next();
             }
         });
-        if (!user) {
-            res.status(401).json({
-                errors: "Unauthorized"
-            }).end();
-        } else {
-            req.user = user;
-            next();
-        }
+
     }
 }
